7ba58a525055cea5fc82a24eb7283ea606ae2206
Alexander Craggs <eyrecraggs@gmail.com>
2021-09-04 13:48:42 +0800

    working result

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'desktop': "url('/img/pattern-background-desktop.svg')",
+        'mobile': "url('/img/pattern-background-mobile.svg')"
+        
+    }
+    },
+  },
+  variants: {
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
